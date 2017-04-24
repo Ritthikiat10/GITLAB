0d02b0bfa7eeb6b808f436d558a06f9efca5c60e
Ritthikiat10 <emperor_ton@hotmail.com>
2017-04-24 15:13:36 +0700

    rzy

--- bloodgroup.c
+++ bloodgroup.c
@@ -5,7 +5,7 @@ void main()
   int i;
   do
   {
-    printf("What is your Father Mother Name? ");
+    printf("What is your Father Name? ");
     scanf("%s", Name);
     do
     {
@@ -44,5 +44,5 @@ void main()
     printf("\nContinue (YES for Yes)? ");
     scanf("%s", answer);
   } while (strcmp(answer, "YES") == 0);
-  printf("Goodbye Sayonara\n");
+  printf("Goodbye Sayo\n");
 }
